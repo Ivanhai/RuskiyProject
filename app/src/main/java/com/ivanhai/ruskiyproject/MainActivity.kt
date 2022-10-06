c0fe8592ef8874f33c1e37e76b069387883ca6d9
bot <12>
2022-10-06 17:13:47 +0700

    idk

--- app/src/main/java/com/ivanhai/ruskiyproject/MainActivity.kt
+++ app/src/main/java/com/ivanhai/ruskiyproject/MainActivity.kt
@@ -9,18 +9,11 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import com.chaquo.python.Python
-import com.chaquo.python.android.AndroidPlatform
-import com.ivanhai.ruskiyproject.spacy.Spacy
 import com.ivanhai.ruskiyproject.ui.theme.RuskiyProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if(!Python.isStarted()) {
-            Python.start(AndroidPlatform(this))
-            Spacy.install()
-        }
         setContent {
             RuskiyProjectTheme {
                 // A surface container using the 'background' color from the theme
@@ -37,8 +30,5 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    val coroutineScope = rememberCoroutineScope()
-    val (text, setText) = remember { mutableStateOf(name) }
-    setText(Spacy.print(name)[0].call("text").toString())
-    Text(text = text)
+    Text(text = name)
 }
